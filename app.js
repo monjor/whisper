2953d77d64b3091d140f372ae74fa0dcd7af9c8f
Abenezer <monjo003@morris.umn.edu>
2020-05-29 15:07:21 -0500

    socket addresse already in use

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
+const mongoose = require("mongoose");
 
 const app = express();
 
@@ -9,6 +10,8 @@ app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({
     extended: true
 }));
+
+mongoose.connect("mongodb://localhost:2701/userDB", {useNewUrlParser: true});
 
 
 app.get('/', function(req, res){
@@ -25,3 +28,7 @@ app.get('/register', function(req, res){
 app.listen(3000, function(){
     console.log("Server started on port 3000");
 });
+
+// Soocket addrese already in use::
+// sudo lsof -iTCP -sTCP:LISTEN -n -P
+// sudo kill <mongo_command_pid>
