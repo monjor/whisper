10850b9d7fc0af4d6814fab9fd7c5573e541c115
Abenezer <monjo003@morris.umn.edu>
2020-05-29 15:17:40 -0500

    adding and saving new user to render the secrets page

--- app.js
+++ app.js
@@ -13,6 +13,17 @@ app.use(bodyParser.urlencoded({
 
 mongoose.connect("mongodb://localhost:2701/userDB", {useNewUrlParser: true});
 
+//creating a user schema
+const userSchema = {
+    email: String,
+    password: String
+};
+
+//using the user schema to set up a new user model
+//"USER" collection created using the user Schema
+
+const User = new mongoose.model("User", userSchema)
+
 
 app.get('/', function(req, res){
     res.render('home');
@@ -24,6 +35,23 @@ app.get('/register', function(req, res){
     res.render('register');
 });
 
+// create user inside the route
+app.post("/register", function(req, res){
+    const newUser = new  User ({
+        email: req.body.username,
+        password: req.body.password
+    });
+
+    newUser.save(function(err){
+        if(err) {
+            console.log(err);
+        }else{
+            res.render("Secret");
+        }
+    });
+
+});
+
 
 app.listen(3000, function(){
     console.log("Server started on port 3000");
@@ -32,3 +60,11 @@ app.listen(3000, function(){
 // Soocket addrese already in use::
 // sudo lsof -iTCP -sTCP:LISTEN -n -P
 // sudo kill <mongo_command_pid>
+
+// monogo server
+// path cd /usr/local/var/mongodb
+// to run : mongod --dbpath /usr/local/var/mongodb
+
+// shell
+// -path cd /usr/local/bin
+// to run: ./mongo 
